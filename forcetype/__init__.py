82dacaa066282eefd6e86a347d3485b06cfaf9f6
Anderson da Cruz <anderson@centrimobi.com.br>
2016-04-23 10:29:29 -0300

    First commit

---
+++ forcetype/__init__.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+#-*- coding: iso-8859-1 -*-
+
+
+import inspect
+from functools import wraps
+# import functools
+# import re
+
+
+def forcetype(method):
+	"""
+
+	:param method:
+	:return:
+	"""
+
+	argspec = inspect.getfullargspec(method)
+
+	if not argspec.annotations:
+		# raise Exception("All parameters need be typed")
+		return method
+
+	# default_arg_count = len(argspec.defaults or [])
+	# non_default_arg_count = len(argspec.args) - default_arg_count
+	# method_name = method.__name__
+	# kwarg_defaults = argspec.kwonlydefaults or {}
+
+	type_list = []
+	param_list = []
+
+	for n, v in argspec.annotations.items():
+
+		type_list.append(v)
+		param_list.append(n)
+
+	def inner(*args, **kwargs):
+		"""
+
+		:param args:
+		:param kwargs:
+		:return:
+		"""
+
+		for i, v in enumerate(args):
+
+			if not isinstance(v, type_list[i]):
+				raise Exception("Param %s need be a %s type" % (param_list[i], type_list[i]))
+
+		result = method(*args, **kwargs)
+
+		if not isinstance(result, type_list[-1]):
+			raise Exception('Type Error')
+
+		return result
+
+		return method(*args, **kwargs)
+
+	return inner
+
+
+@forcetype
+def add(op1: int, op2: int) -> int:
+	"""
+	Add
+	:param op1:
+	:param op2:
+	:return:
+	"""
+	return op1 + op2
+
+
+def subtract(op1, op2):
+	"""
+
+	:param op1:
+	:param op2:
+	:return:
+	"""
+	return op1 - op2
+
+if __name__ == '__main__':
+
+	print("Starting ...")
+
+	print(">>>>", add(1, 2))
+	# print(">>>>", subtract(1, '2'))
+
+	print("Finish.")
