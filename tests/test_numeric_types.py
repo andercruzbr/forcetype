ff6f1a2ec9ee4be09044d9b4b127ad8f66f2a5cf
Anderson da Cruz <anderson@centrimobi.com.br>
2016-04-30 00:07:45 -0300

    Added numeric test cases

--- tests/test_numeric_types.py
+++ tests/test_numeric_types.py
@@ -1,20 +1,26 @@
 from unittest.case import TestCase
+from unittest.mock import patch
 
 from numbers import Number
 
 from decimal import Decimal
 
-from forcetype import forcetype
+from forcetype import forcetype, WrongParameterTypeException, WrongReturnTypeException
 
 
 @forcetype
-def add(op1: Number, op2: Number)-> Number:
+def add_number(op1: Number, op2: Number)-> Number:
 	"""
 	add
 	:param op1:
 	:param op2:
 	:return:
 	"""
+	result = op1 + op2
+
+	if result > 999:
+		return str(result)
+
 	return op1 + op2
 
 @forcetype
@@ -25,7 +31,13 @@ def add_decimal(op1: Decimal, op2: Decimal)-> Decimal:
 	:param op2:
 	:return:
 	"""
-	return op1 + op2
+
+	result = op1 + op2
+
+	if result > 999:
+		return float(result)
+
+	return result
 
 @forcetype
 def add_float(op1: float, op2: float)-> float:
@@ -35,15 +47,24 @@ def add_float(op1: float, op2: float)-> float:
 	:param op2:
 	:return:
 	"""
-	return op1 + op2
+	result = op1 + op2
 
+	if result > 999:
+		return int(result)
 
-def convert_to_decimal(value: 1) -> Decimal:
+	return  result
+
+@forcetype
+def convert_to_decimal(value: int) -> Decimal:
 	"""
 	convert_to_decimal
 	:param value:
 	:return:
 	"""
+
+	if value == 9:
+		return value
+
 	return Decimal(value)
 
 
@@ -55,5 +76,38 @@ class TestNumericTypes(TestCase):
 	def tearDown(self):
 		pass
 
-	def test_01(self):
-		pass
+	def test_numeric_success(self):
+
+		self.assertEquals(add_number(1.1, 3), 4.1)
+
+		self.assertEquals(add_number(4, 3.9), 7.9)
+
+		self.assertEquals(add_decimal(Decimal(3), Decimal(9)), Decimal(12))
+
+		self.assertEquals(add_decimal(Decimal(1), Decimal(5)), Decimal('6'))
+
+		self.assertEquals(add_float(.5, .9), 1.4)
+
+		self.assertEquals(add_float(1.0, 2.0), 3.0)
+
+	def test_numeric_parametes(self):
+
+		self.assertRaises(WrongParameterTypeException, lambda: add_number('1', 1))
+
+		self.assertRaises(WrongParameterTypeException, lambda: add_decimal(Decimal(1), 1))
+
+		self.assertRaises(WrongParameterTypeException, lambda: add_float(1000, 1.2))
+
+		self.assertRaises(WrongParameterTypeException, lambda: convert_to_decimal(3.))
+
+
+	def test_numeric_return(self):
+
+		self.assertRaises(WrongReturnTypeException, lambda: add_number(1.2, 1000))
+
+		self.assertRaises(WrongReturnTypeException, lambda: add_decimal(Decimal(10000), Decimal(1)))
+
+		self.assertRaises(WrongReturnTypeException, lambda: add_float(10000.0, 1.0))
+
+		self.assertRaises(WrongReturnTypeException, lambda: convert_to_decimal(9))
+
